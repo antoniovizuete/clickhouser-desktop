52589c6798f9c5f85547dddbe5dd41f7e9708091
Antonio Vizuete <antonio.vizuete@lansweeper.com>
2023-03-21 23:29:55 +0100

    Be able to remove saved queries

--- src/components/SideBars/SavedQueriesSideBar.tsx
+++ src/components/SideBars/SavedQueriesSideBar.tsx
@@ -1,15 +1,22 @@
-import { Icon } from "@blueprintjs/core";
+import { Button, Classes, Dialog, Icon } from "@blueprintjs/core";
 import { useEffect, useState } from "react";
 import { useTabsContext } from "../../contexts/useTabsContext";
+import { useThemeContext } from "../../contexts/useThemeContext";
 import { useSaveQueryEvent } from "../../hooks/useSaveQueryEvent";
 import { Query, queryRepo } from "../../lib/backend-repos/query-repo";
+import { AppToaster } from "../../lib/toaster/AppToaster";
 import ClickableIcon from "../core/ClickableIcon";
 
 
 export default function SavedQueriesSideBar() {
+  const { bpTheme } = useThemeContext();
   const { restoreTab } = useTabsContext();
-  const [queries, setQueries] = useState<Query[]>([]);
   const { useSaveQueryEventListen } = useSaveQueryEvent();
+
+  const [queries, setQueries] = useState<Query[]>([]);
+  const [queryToRemove, setQueryToRemove] = useState<Query | undefined>();
+  const [isAlertOpen, setIsAlertOpen] = useState(false);
+
 
   const retrieveQueries = async () => {
     const rawQueries = await queryRepo.get();
@@ -24,11 +31,28 @@ export default function SavedQueriesSideBar() {
     retrieveQueries();
   }, []);
 
+  const handleOnClickRemoveQuery = (query: Query) => {
+    setQueryToRemove(query);
+    setIsAlertOpen(true);
+  }
+
   const handleOnClickQuery = (query: Query) => {
     restoreTab(query)
   }
 
-  return (
+  const closeAlert = () => {
+    setIsAlertOpen(false);
+  }
+
+  const handleOnClickConfirmRemoveQuery = async () => {
+    if (queryToRemove) {
+      queryRepo.delete(queryToRemove.id);
+      retrieveQueries();
+      closeAlert();
+      AppToaster.top.warn(`Query "${queryToRemove.name}" removed`);
+    }
+  }
+  return (<>
     <div className="w-full h-full pt-2 flex flex-col justify-start items-start bg-stone-50 dark:bg-stone-900">
       <h2 className="pl-4 py-3 text-stone-600 dark:text-stone-300 uppercase text-xs font-semibold">Saved queries</h2>
       <div className="pl-2 flex-grow flex flex-col justify-start items-start w-full">
@@ -41,10 +65,36 @@ export default function SavedQueriesSideBar() {
               <Icon icon="console" className="mr-2" />
               {query.name}
             </div>
-            <ClickableIcon icon="trash" className="opacity-0 group-hover:opacity-100 hover:scale-110 hover:text-red-600" />
+            <ClickableIcon
+              icon="delete"
+              className="opacity-0 group-hover:opacity-100 hover:scale-110 hover:text-red-600"
+              onClick={() => handleOnClickRemoveQuery(query)}
+            />
           </div>
         ))}
       </div>
     </div>
+    <Dialog
+      icon="warning-sign"
+      className={bpTheme}
+      isOpen={isAlertOpen}
+      onClose={closeAlert}
+      title="Unsaved query"
+    ><div className={`${Classes.DIALOG_BODY} ${bpTheme} flex flex-col gap-3`}>
+
+        <div>You are going to remove the "<span className='font-bold'>{queryToRemove?.name}</span>" query. The query data will be lost after clicking on 'Yes, remove it'.</div>
+
+        <div>Are you sure you want to remove the query?</div>
+      </div><div className={Classes.DIALOG_FOOTER}>
+        <div className={Classes.DIALOG_FOOTER_ACTIONS}>
+          <Button onClick={closeAlert} type={"button"}>
+            Cancel
+          </Button>
+          <Button intent="danger" type="submit" onClick={handleOnClickConfirmRemoveQuery}>
+            Yes, remove it
+          </Button>
+        </div>
+      </div></Dialog>
+  </>
   )
 }
