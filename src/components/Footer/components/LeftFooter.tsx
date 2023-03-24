1de0e79685a4c5a4a27476b2d9e4893b7ccfb19d
Antonio Vizuete <antonio.vizuete@lansweeper.com>
2023-03-24 06:57:27 +0100

    Fix footer

--- src/components/Footer/components/LeftFooter.tsx
+++ src/components/Footer/components/LeftFooter.tsx
@@ -2,7 +2,7 @@ import { Icon, Menu } from "@blueprintjs/core";
 import { MenuItem2, Popover2, Tooltip2 } from "@blueprintjs/popover2";
 import classNames from "classnames";
 import { useThemeContext } from "../../../contexts/useThemeContext";
-import { JsonResult } from "../../../lib/clickhouse-clients";
+import { isJsonResult, QueryResult } from "../../../lib/clickhouse-clients";
 import { download, KindEnum } from "../../../lib/file-downloader";
 import {
   formatReadableBytes,
@@ -10,27 +10,33 @@ import {
 } from "../../../lib/stats-helpers/format-readable";
 
 type Props = {
-  result: JsonResult;
+  result?: QueryResult;
 };
 
 export default function LeftFooter({ result }: Props) {
   const { bpTheme } = useThemeContext();
+
+  if (!isJsonResult(result)) {
+    return null;
+  }
+
   const { statistics, data } = result;
   const handleDownload = (kind: KindEnum) => () => {
     download(result, kind);
   };
+
   return (
     <div className={classNames(
-      "flex flex-row justify-start items-center gap-2 divide-x divide-neutral-300 dark:divide-neutral-500"
+      "flex flex-row justify-start items-center gap-1 divide-x divide-neutral-300 dark:divide-neutral-500"
     )}>
-      <div className="">Elapsed: {statistics.elapsed.toFixed(2)} s.</div>
-      <div className="">{formatReadableRows(data.length)} rows</div>
-      <div className="">
+      <div className="stat">Elapsed: {statistics.elapsed.toFixed(2)} s.</div>
+      <div className="stat">{formatReadableRows(data.length)} rows</div>
+      <div className="stat">
         {formatReadableRows(statistics.rows_read)} read rows
       </div>
-      <div className="">{formatReadableBytes(statistics.bytes_read)}</div>
+      <div className="stat">{formatReadableBytes(statistics.bytes_read)}</div>
       {false && data.length > 0 && (
-        <div className=" bg-[#fbb360]">
+        <div className="stat bg-[#fbb360]">
           <Popover2
             popoverClassName={bpTheme}
             position="top"
